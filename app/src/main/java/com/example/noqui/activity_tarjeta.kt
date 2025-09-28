5c1f89e28bb66e3756a481717c7c527153c07722
Nailu07 <nailu0793@gmail.com>
2025-09-28 11:59:05 -0500

    Tarjeta, congelar tarjeta y Mov de Tarjeta 2.0

--- app/src/main/java/com/example/noqui/activity_tarjeta.kt
+++ app/src/main/java/com/example/noqui/activity_tarjeta.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.content.Intent
+import android.widget.ImageButton
+
 
 class activity_tarjeta : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tarjeta)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val numTarj = "1111222233334444"
+        val expiraTj = "11/28"
+        val codigoTj = "123"
+
+        val numTarjeta = findViewById<TextView>(R.id.numTarjeta)
+        val expiraTarjeta = findViewById<TextView>(R.id.expTj)
+        val codTarjeta = findViewById<TextView>(R.id.codTj)
+        numTarjeta.text = numTarj
+        expiraTarjeta.text = expiraTj
+        codTarjeta.text = codigoTj
+
+        val btnMovTj = findViewById<ImageButton>(R.id.btnImgMovTarjeta)
+        val btnCongTj = findViewById<ImageButton>(R.id.btnImgCongTarjeta)
+
+        btnMovTj.setOnClickListener {
+            val intent = Intent(this, activity_tarjeta_movimientos::class.java)
+            startActivity(intent)
+        }
+        btnCongTj.setOnClickListener {
+            val intent = Intent(this, activity_congelar_tarjeta::class.java)
+            startActivity(intent)
+        }
+
+
         }
     }
-}
--- app/src/main/java/com/example/noqui/activity_tarjeta_movimientos.kt
+++ app/src/main/java/com/example/noqui/activity_tarjeta_movimientos.kt
@@ -1,6 +1,7 @@
 package com.example.noqui
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,11 @@ class activity_tarjeta_movimientos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tarjeta_movimientos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnVolver = findViewById<ImageButton>(R.id.botonVolverMovmTj)
+
+        btnVolver.setOnClickListener {
+            finish()
         }
     }
 }
